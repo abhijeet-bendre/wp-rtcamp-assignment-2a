7e5a910431a7da5fb8e85cec29518062ca41e695
abhijeet <abhijeet@tymescripts.com>
2017-09-12 13:23:07 +0530

    Tests if slideshow post meta is saved

--- tests/test-wp-rtcamp-assignment-2a.php
+++ tests/test-wp-rtcamp-assignment-2a.php
@@ -10,6 +10,7 @@
  */
 class Wp_Rtcamp_Assignment_2a_Test extends WP_UnitTestCase {
 
+	protected static $post_type = 'wprtc_slideshow';
 	/**
 	 * Setup of 'setUpBeforeClass' test fixture
 	 */
@@ -17,8 +18,7 @@ class Wp_Rtcamp_Assignment_2a_Test extends WP_UnitTestCase {
 		// Call parent's setUpBeforeClass method.
 		parent::setUpBeforeClass();
 
-		$post_type = 'wprtc_slideshow';
-		register_post_type( $post_type,
+		register_post_type( self::$post_type,
 			array(
 				'supports' => array(
 					'title',
@@ -41,4 +41,26 @@ class Wp_Rtcamp_Assignment_2a_Test extends WP_UnitTestCase {
 		$this->assertTrue( post_type_exists( 'wprtc_slideshow' ) );
 	}
 
+	/**
+	 * Test if slideshow post meta is saved
+	 */
+	function test_if_slideshow_post_meta_is_saved() {
+		// Simulate $_POST variable for save_post hook.
+		$_POST['post_type'] = self::$post_type;
+
+		// Simulate $_POST with Slides for fake attachment id for .eg. 180.
+		$_POST['wprtc_slide_order'] = array( 180 );
+		$post_id  = $this->factory()->post->create(
+			array(
+				'post_status' => 'publish',
+				'post_title' => 'Post Title ',
+				'post_type' => 'wprtc_slideshow',
+			)
+		);
+
+		$slider_images = get_post_meta( $post_id, '_wprtc_slideshow_slides' );
+
+		$slider_images = $slider_images[0];
+		$this->assertEquals( 180, $slider_images[0] );
+	}
 }
