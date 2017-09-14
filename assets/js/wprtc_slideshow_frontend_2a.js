03adec2ab9e2c9f4a3b9cca21361516478dd41d0
abhijeet <abhijeet@tymescripts.com>
2017-09-14 18:03:14 +0530

    added frontend js file  'wprtc_slideshow_frontend_2a.js'

---
+++ assets/js/wprtc_slideshow_frontend_2a.js
@@ -0,0 +1,11 @@
+
+jQuery(document).ready(function(){
+      // Hook up the flexslider
+      jQuery('.flexslider').flexslider({
+          animation: "fade",
+          direction: "horizontal",
+          slideshowSpeed: 7000,
+          animationSpeed: 600
+        });
+
+});
