e2df0c547627081b4b4fb30f054c349725366f3a
abhijeet <abhijeet@tymescripts.com>
2017-09-23 10:50:10 +0530

    Test if slideshow setting "animation_type : slide" is saved.

--- tests/test-wp-rtcamp-slider-settings-assignment-2a-.php
+++ tests/test-wp-rtcamp-slider-settings-assignment-2a-.php
@@ -48,9 +48,9 @@ class Wp_Rtcamp_Slider_Settings_Assignment_2a_Test extends WP_UnitTestCase {
 	/**
 	 * Test if slideshow setting "animation_type : fade" is saved.
 	 */
-	function test_if_wprtc_slideshow_slides_are_saved() {
+	function test_if_wprtc_slideshow_setting_animation_type_fade_is_saved() {
 
-		// Simulate $_POST with Slides for fake attachment id (for eg. 180).
+		// Simulate $_POST with slider setting animation_type = fade.
 		$_POST['_wprtc_slider_settings[animation_type]'] = array( 'fade' );
 
 		$post_id  = $this->factory()->post->create(
@@ -65,5 +65,27 @@ class Wp_Rtcamp_Slider_Settings_Assignment_2a_Test extends WP_UnitTestCase {
 
 		$slider_settings = $slider_settings[0];
 		$this->assertEquals( 'fade', $slider_settings[0] );
+	}
+
+	/**
+	 * Test if slideshow setting "animation_type : slide" is saved.
+	 */
+	function test_if_wprtc_slideshow_setting_animation_type_slide_is_saved() {
+
+		// Simulate $_POST with slider setting animation_type = slide.
+		$_POST['_wprtc_slider_settings[animation_type]'] = array( 'slide' );
+
+		$post_id  = $this->factory()->post->create(
+			array(
+				'post_status' => 'publish',
+				'post_title' => 'Post Title ',
+				'post_type' => 'wprtc_slideshow',
+			)
+		);
+
+		$slider_settings = get_post_meta( $post_id, '_wprtc_slideshow_settings' );
+
+		$slider_settings = $slider_settings[0];
+		$this->assertEquals( 'slide', $slider_settings[0] );
 	}
 }
